8b33755fb06c1de6863c02c54cc03484961bfe62
tako0910 <tako.oct0910@gmail.com>
2014-06-21 22:16:18 +0900

    valentewx: fix ifneq for wcnss_htc

--- wcnss_htc/Android.mk
+++ wcnss_htc/Android.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-ifneq ($(filter jewel evita ville fireball,$(TARGET_DEVICE)),)
+ifneq ($(filter jewel evita ville fireball valentewx,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
